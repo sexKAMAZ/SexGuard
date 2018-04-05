e3b9277afe24b394c8bb73a0b848e91564f1cea1
Alexander <20995281+sexKAMAZ@users.noreply.github.com>
2018-04-05 23:20:48 +0300

    Add SexQLite

---
+++ src/sex/guard/util/SexQLite.php
@@ -0,0 +1,156 @@
+<?php namespace sex\guard\util;
+
+
+/**
+ *  _    _       _                          _  ____
+ * | |  | |_ __ (_)_    _____ _ ______ __ _| |/ ___\_ _______      __
+ * | |  | | '_ \| | \  / / _ \ '_/ __// _' | | /   | '_/ _ \ \    / /
+ * | |__| | | | | |\ \/ /  __/ | \__ \ (_) | | \___| ||  __/\ \/\/ /
+ *  \____/|_| |_|_| \__/ \___|_| /___/\__,_|_|\____/_| \___/ \_/\_/
+ *
+ * @author sex_KAMAZ
+ * @link   https://vk.com/infernopage
+ *         https://t.me/sex_kamaz
+ *
+ */
+use Exception;
+use InvalidArgumentException;
+
+use SQLite3Result;
+use SQLite3Stmt;
+use SQLite3;
+
+
+class SexQLite
+{
+	/**
+	 *  _____            ___  _    _ _
+	 * / ___/  _____  __/ _ \| |  (_) |_____
+	 * \___ \ / _ \ \/ / | | | |  | | __/ _ \
+	 *  ___) |  __/>  <| |_| | |__| | ||  __/
+	 * /____/ \___/_/\_\\__,_\____|_|\__\___/
+	 *
+	 *
+	 * @param  string $database
+	 *
+	 * @return SQLite3
+	 */
+	static function connect( string $database ): SQLite3
+	{
+		return new SQLite3($database);
+	}
+
+
+	/**
+	 * @param  SQLite3 $link
+	 *
+	 * @return bool
+	 *
+	 * @throws Exception
+	 */
+	static function close( SQLite3 $link ): bool
+	{
+		if( $link->close() )
+		{
+			return true;
+		}
+
+		throw new Exception("SexQLite error: trying to close connection.");
+	}
+
+
+	/**
+	 * @param  SQLite3 $link
+	 * @param  string  $sql
+	 *
+	 * @return SQLite3Stmt
+	 */
+	static function prepare( SQLite3 $link, string $sql ): SQLite3Stmt
+	{
+		return $link->prepare($sql);
+	}
+
+
+	/**
+	 * @param  SQLite3Stmt $statement
+	 * @param  string      $param
+	 * @param  mixed       $value
+	 *
+	 * @return bool
+	 *
+	 * @throws Exception
+	 */
+	static function bind( SQLite3Stmt $statement, string $param, $value ): bool
+	{
+		if( $statement->bindValue($param, self::type($value)) )
+		{
+			return true;
+		}
+
+		throw new Exception("SexQLite error: trying to bind $value in $param.");
+	}
+
+
+	/**
+	 * @param  SQLite3Stmt $statement
+	 *
+	 * @return SQLite3Result
+	 */
+	static function execute( SQLite3Stmt $statement ): SQLite3Result
+	{
+		return $statement->prepare($statement);
+	}
+
+
+	/**
+	 * @param  SQLite3Result $result
+	 *
+	 * @return mixed[]
+	 */
+	static function fetch( SQLite3Result $result ): array
+	{
+		$array = $result->fetchArray(SQLITE3_ASSOC);
+
+		if( !$array )
+		{
+			return [];
+		}
+
+		return $array;
+	}
+
+
+	/**
+	 * @param  SQLite3Result $result
+	 *
+	 * @return int
+	 */
+	static function num( SQLite3Result $result ): int
+	{
+		return $result->numColumns();
+	}
+
+
+	/**
+	 * @param  mixed $value
+	 *
+	 * @return int
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	private static function type( $value ): int
+	{
+		$type = gettype($value);
+
+		switch( $type )
+		{
+			case 'double':  return SQLITE3_FLOAT;
+			case 'integer': return SQLITE3_INTEGER;
+			case 'boolean': return SQLITE3_INTEGER;
+			case 'NULL':    return SQLITE3_NULL;
+			case 'string':  return SQLITE3_TEXT;
+		}
+
+		throw new InvalidArgumentException("SexQLite error: Invalid type '$type'.");
+	}
+}
